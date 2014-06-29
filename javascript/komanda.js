13e575212743dce6126c4329e6256557599f73dc
Dustin Willis Webber <dustin.webber@gmail.com>
2014-06-29 14:51:25 -0400

    update

--- javascript/komanda.js
+++ javascript/komanda.js
@@ -1,4 +1,21 @@
+var OSName="Unknown OS";
+if (navigator.appVersion.indexOf("Win")!=-1) OSName="Windows";
+if (navigator.appVersion.indexOf("Mac")!=-1) OSName="MacOS";
+if (navigator.appVersion.indexOf("X11")!=-1) OSName="UNIX";
+if (navigator.appVersion.indexOf("Linux")!=-1) OSName="Linux";
+
 jQuery(document).ready(function($) {
 
+  if (OSName === "Windows") {
+    $('button.download').hide();
+    $('button.download.windows').show();
+  } else if (OSName === "MacOS") {
+    $('button.download.apple').show();
+  } else if (OSName === "Linux") {
+    $('button.download').hide();
+    $('button.download.linux').show();
+  } else {
+    $('button.download.apple').show();
+  }
 
 });
